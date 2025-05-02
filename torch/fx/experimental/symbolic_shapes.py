516239b4d8446c359f11e7cba98fd3a6664f7474
bobrenjc93 <bobren@meta.com>
2025-05-01 23:36:33 -0700

    use backend specializations in compile_and_call_fx_graph
    ghstack-source-id: 6323b34d0d393b6b1390aa0e2e475e893b0f6beb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152601

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -937,6 +937,12 @@ def find_symbol_binding_fx_nodes(
     return r
 
 
+@dataclass
+class BackendSpecialization:
+    symbol: sympy.Symbol
+    hint: int
+    specialization: Callable
+
 # Analogous to ConvertIntSource
 @dataclass(frozen=True)
 class ConvertIntKey:
@@ -3567,6 +3573,8 @@ class ShapeEnv:
 
         self.trace_asserts = trace_asserts
 
+        self.backend_specializations = []
+
         from torch.fx.experimental.validator import translation_validation_enabled
 
         self._translation_validation_enabled = translation_validation_enabled()
@@ -4050,6 +4058,11 @@ class ShapeEnv:
                 do_not_specialize_zero_one=config.backed_size_oblivious,
                 symbolic_context=symbolic_context,
             )
+            for specialization in symbolic_context.backend_specializations:
+                self.backend_specializations.append(BackendSpecialization(
+                    sym,
+                    *specialization,
+                ))
             if (
                 config.backed_size_oblivious
                 and isinstance(sym, sympy.Symbol)  # could be static
@@ -4148,6 +4161,7 @@ class ShapeEnv:
         source: Source,
         *,
         symbolic_context: Optional[SymbolicContext] = None,
+        specialization = None,
     ) -> tuple[tuple[IntLikeType, ...], tuple[IntLikeType, ...], IntLikeType,]:
         dim = len(ex_size)
 
@@ -4226,6 +4240,11 @@ class ShapeEnv:
             )
             for i, (sym, hint) in enumerate(zip(size, ex_size))
         ]
+
+        for i, size in enumerate(sym_sizes):
+            if i in specialization.idxs:
+                expect_true(specialization.lambdas[i](size))
+
         sym_stride = []
         for i, stride_expr in enumerate(stride):
             # NB: Don't duck size the stride; instead use the expression
